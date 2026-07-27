5611dc22e25e5916b786aaaaf7a6d4bd8806790c
LissaMungai <71770064+Melissa907@users.noreply.github.com>
2026-07-27 23:09:44 +0300

    Update constants.php

--- database/constants.php
+++ database/constants.php
@@ -2,7 +2,7 @@
     $host = "127.0.0.1";
     $username = "root";
     $password = "";
-    $database = "is_project_database";
+    $database = "is_project_database"; 
 
     $conn = new mysqli($host, $username, $password, $database);
 ?>

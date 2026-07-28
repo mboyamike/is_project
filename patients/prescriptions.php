68095865d3bfe73208378808795072a5d9a8fe77
LissaMungai <71770064+Melissa907@users.noreply.github.com>
2026-07-28 12:09:55 +0300

    Update prescriptions.php

--- patients/prescriptions.php
+++ patients/prescriptions.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    if(!isset($_SESSION['user_type'])) { header("location: ../sign_in.php"); } 
+    if(!isset($_SESSION['user_type'])) { header("location: ../sign_in.php"); }  
     include("../database/constants.php");
 ?>
 
